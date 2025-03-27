3f36ed00d5711debfbeda00dd4ea9317bcd43724
elenapct <elenapenacastillo@gmail.com>
2025-03-27 17:05:40 +0100

    update functions

--- colores_team_1.py
+++ colores_team_1.py
@@ -3,7 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 
-# Paleta de colores personalizada - paleta team 1 (sin blanco ni negro)
+# DEfinimos los colores que forman parte de la paleta personalizada
 paleta_team_1 = [
     "#DA878E",  # Rosa suave
     "#E99A7B",  # Salmón
@@ -11,18 +11,14 @@ paleta_team_1 = [
     "#9EBE99",  # Verde suave
     "#91B0BC",  # Azul grisáceo
     "#9E81BA",  # Morado pastel
-    "#D994BF"   # Rosa pastel
-]
+    "#D994BF"]   # Rosa pastel
 
+#creamos la funcion para decirle a seaborn que use la pelatela personalizada por defecto.
 def apply_team_palette():
-    """
-    Apply the base team palette to seaborn
-    """
     sns.set_palette(paleta_team_1)
-    print("Team palette applied (white and black removed)")
+    print("Team palette applied")
 
+#en los casos en los que necesitemos más colores de los 7 colores que tiene la paleta personaliza, 
+#creamos un degradado de colores con una trancision suave (N=256 como parametro)
 def get_gradient_cmap(name="team_palette_gradient", N=256):
-    """
-    Generate a gradient colormap from the base palette for heatmaps or large color needs
-    """
     return LinearSegmentedColormap.from_list(name, paleta_team_1, N=N)
